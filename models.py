9563cc25a47ec51c14d901b8289ac31e84fa1deb
KlunnyS <Tadeas.Nevrela@azet.sk>
2026-06-15 09:04:27 +0200

    Update models.py

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(512), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
     profile_image_url = db.Column(db.String(120), nullable=True)
     public_display_name = db.Column(db.String(80), nullable=True)
     public_bio = db.Column(db.Text, nullable=True)
